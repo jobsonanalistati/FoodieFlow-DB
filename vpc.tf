6525042e8cdf5e2dc80c25572c6ab06389167e5e
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-10 21:55:13 -0300

    Deus-ajuda-tf

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name                 = "VPC-${var.projectName}"
   cidr                 = "172.31.0.0/16"
   azs                  = data.aws_availability_zones.available.names
-  public_subnets       = ["172.31.32.0/20", "172.31.0.0/20", "172.31.80.0/20"]
+  public_subnets       = ["172.31.80.0/20", "172.31.16.0/20", "172.31.32.0/20"]
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
