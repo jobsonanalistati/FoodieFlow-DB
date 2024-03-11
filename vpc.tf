b505ce231e5a4e7f4a2be2a519b4b9bf90faf84a
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-10 22:56:42 -0300

    ajustes-aws-tf

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,13 @@
+data "aws_availability_zones" "available" {
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
 
   name                 = "VPC-${var.projectName}"
   cidr                 = "172.31.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
+  azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
   public_subnets       = ["172.31.80.0/20", "172.31.16.0/20", "172.31.32.0/20"]
   enable_dns_hostnames = true
   enable_dns_support   = true
