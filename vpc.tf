1fca309cdb5f2545513cb31dfd404b8e6c6192c5
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-10 23:06:01 -0300

    version-tf

--- vpc.tf
+++ vpc.tf
@@ -3,12 +3,14 @@ data "aws_availability_zones" "available" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.2.0"
+  version = "5.5.2"
 
-  name           = "VPC-${var.projectName}"
-  cidr           = "172.31.0.0/16"
-  azs            = slice(data.aws_availability_zones.available.names, 0, 3)
-  public_subnets = ["172.31.80.0/20", "172.31.16.0/20", "172.31.32.0/20"]
+  name                 = "VPC-${var.projectName}"
+  cidr                 = "172.31.0.0/16"
+  azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets       = ["172.31.80.0/20", "172.31.16.0/20", "172.31.32.0/20"]
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 resource "aws_db_subnet_group" "FoodieFlowVPC" {
