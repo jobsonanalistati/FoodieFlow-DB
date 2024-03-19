8fc0674344f0a9a143ea1e5d5addaa7366514503
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-19 00:00:21 -0300

    ajuste-vpc

--- vpc.tf
+++ vpc.tf
@@ -21,5 +21,31 @@ resource "aws_db_subnet_group" "FoodieFlowVPC-rds" {
     Name = "FoodieFlowVPC-rds"
   }
 }
+data "aws_availability_zones" "available" {
+}
 
-#vpc pelo cidr pela aws do jobson
+
+resource "aws_security_group" "rds-aws-security-group" {
+  name        = "rds-production-security-group"
+  description = "Grupo de segurança para o RDS em produção"
+
+  vpc_id = module.vpc.vpc_id  # Usando o ID da VPC do módulo
+
+  // Regra de entrada para IPv6 permitindo todos os TCPs de qualquer lugar
+  ingress {
+    description = "Permitir tráfego IPv6 TCP de qualquer lugar"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  // Regra de entrada para IPv4 permitindo todos os TCPs de qualquer lugar
+  ingress {
+    description = "Permitir tráfego IPv4 TCP de qualquer lugar"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
