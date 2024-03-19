21ffdf03d04fdc28bac0aad9fb7b4de4d342e646
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-03-19 07:16:33 -0300

    ajuste-scret-07h

--- secret_manager.tf
+++ secret_manager.tf
@@ -1,9 +1,9 @@
-resource "aws_secretsmanager_secret" "foodieFlow" {
-  name = "foodieFlow"
+resource "aws_secretsmanager_secret" "foodieFlow_app" {
+  name = "foodieFlow_app"
 }
 
-resource "aws_secretsmanager_secret_version" "foodieFlow" {
-  secret_id = aws_secretsmanager_secret.foodieFlow.id
+resource "aws_secretsmanager_secret_version" "foodieFlow_app" {
+  secret_id = aws_secretsmanager_secret.foodieFlow_app.id
   secret_string = jsonencode({
     host     = aws_db_instance.FoodieFlowRds.address,
     db_name  = var.POSTGRES_DB,
